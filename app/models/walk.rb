2b4f1988081d6c47ada712f1e8bc9ace97c07f30
Hunter Bourgeois <bahunter@gmail.com>
2021-11-23 16:59:26 +0100

    most pages and routes done

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -1,6 +1,8 @@
 class Walk < ApplicationRecord
   belongs_to :creator, foreign_key: :creator_id, primary_key: :id, class_name: "User"
   belongs_to :category
+  has_many :completed_walks
   has_many :users, through: :completed_walks
+  has_many :walk_landmarks
   has_many :landmarks, through: :walk_landmarks
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/walks/:walk_id/:landmark_order_no', to: 'walks#navigate'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :walks, only: [:show, :index] do
     resources :landmarks, only: [:show, :index]
