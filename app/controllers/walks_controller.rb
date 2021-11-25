d6dc116471e849bc870da4a17510de128c42410b
Hunter Bourgeois <79214781+bahunter105@users.noreply.github.com>
2021-11-25 19:46:44 +0100

    Update walks_controller.rb
    cleaning up some text

--- app/controllers/walks_controller.rb
+++ app/controllers/walks_controller.rb
@@ -6,7 +6,7 @@ class WalksController < ApplicationController
   def show
     @walk = Walk.find(params[:id])
     @landmarks = @walk.landmarks
-    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    # the `geocoded` scope filters only landmarks with coordinates (latitude & longitude)
     # @markers = @landmarks.geocoded.map do |landmark|
     #   {
     #     lat: landmark.latitude,
@@ -21,19 +21,6 @@ class WalksController < ApplicationController
         info_window: render_to_string(partial: "info_window", locals: { landmark: landmark })
       }
     end
-
-    # base_url= "https://api.mapbox.com/directions/v5/mapbox/walking/"
-    # index = 0
-    # 25.times do
-    #   index += 1
-    #   target_lm = @walk.walk_landmarks.find_by(landmark_order_no: index)
-    #   unless target_lm.nil?
-    #     base_url += "#{target_lm.longitude},#{target_lm.latitude};"
-    #   end
-    # end
-    # api_setting_url = "?alternatives=false&continue_straight=true&geometries=geojson&overview=simplified&steps=false&access_token="
-    # mapbox_key = ENV['MAPBOX_API_KEY']
-    # map_api_url = base_url + api_setting_url + mapbox_key
   end
 
   def navigate
@@ -46,13 +33,6 @@ class WalksController < ApplicationController
          info_window: render_to_string(partial: "info_window", locals: { landmark: @landmark })
        }
     ]
-    # base_url= "https://api.mapbox.com/directions/v5/mapbox/walking/"
-    # current_location = [-3.0000, 40.000]
-    # base_url += "#{current_location[0]}%#{current_location[1]};#{@landmark.longitude}%#{@landmark.latitude}"
-    # api_setting_url = "?alternatives=false&continue_straight=true&geometries=geojson&overview=simplified&steps=false&access_token="
-    # mapbox_key = ENV['MAPBOX_API_KEY']
-    # @map_api_url = base_url + api_setting_url + mapbox_key
-
   end
 
   def start_navigate
