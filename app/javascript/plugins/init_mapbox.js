cf55e09bb3a7c9e6e8500aa30db55b9e596900c4
Hunter Bourgeois <bahunter@gmail.com>
2021-11-25 16:21:28 +0100

    got our location and first landmark on map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -24,27 +24,65 @@ const fitMapToMarkers = (map, markers) => {
   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 2000  });
 };
 
+// const initMapbox = () => {
+//   const mapElement = document.getElementById('map');
+//   if (mapElement) {
+//     const map = buildMap(mapElement);
+//     const markers = JSON.parse(mapElement.dataset.markers);
+//     if (mapElement.dataset.needUserMarker === 'true') {
+//       navigator.geolocation.getCurrentPosition((position) => {
+//         let lat = position.coords.latitude
+//         let lng = position.coords.longitude;
+//         let currentCoords = {
+//           lng: lng,
+//           lat: lat,
+//           info_window: '<h1>This is you</h1>'
+//         };
+//         markers.unshift(currentCoords);
+//         console.log(markers);
+//       });
+//     }
+//     addMarkersToMap(map, markers);
+//     fitMapToMarkers(map, markers);
+//   }
+// };
+
 const initMapbox = () => {
+//   const mapElement = document.getElementById('map');
   const mapElement = document.getElementById('map');
   if (mapElement) {
-    const map = buildMap(mapElement);
     const markers = JSON.parse(mapElement.dataset.markers);
-    if (mapElement.dataset.needUserMarker === 'true') {
-      navigator.geolocation.getCurrentPosition((position) => {
-        let lat = position.coords.latitude
-        let lng = position.coords.longitude;
+    let map
+    if (("geolocation" in navigator) && (mapElement.dataset.needUserMarker === 'true')) {
+
+      navigator.geolocation.getCurrentPosition(position => {
         let currentCoords = {
-          lng: lng,
-          lat: lat,
-          info_window: '<h1>This is you</h1>'
-        };
+            'lng': position.coords.longitude,
+            'lat': position.coords.latitude,
+            'info_window': '<h1>This is you</h1>'
+          };
+
         markers.unshift(currentCoords);
-        console.log(markers);
-      });
+        mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+        map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/streets-v10',
+          center: [currentCoords.lng, currentCoords.lat],
+          zoom: 14
+        })
+
+        addMarkersToMap(map, markers);
+        fitMapToMarkers(map, markers);
+      })
+      console.log(markers);
+
+      // markers.push(currentCoords);
+    } else {
+      map = buildMap(mapElement);
+      addMarkersToMap(map, markers);
+      fitMapToMarkers(map, markers);
     }
-    addMarkersToMap(map, markers);
-    fitMapToMarkers(map, markers);
   }
-};
+}
 
 export { initMapbox };
