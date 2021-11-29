c47bba018f5240f1814fb7931d940d77f5a8d544
Hunter Bourgeois <bahunter@gmail.com>
2021-11-29 16:42:27 +0100

    added distance, but not completely working

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class Walk < ApplicationRecord
   belongs_to :creator, foreign_key: :creator_id, primary_key: :id, class_name: "User"
   belongs_to :category
@@ -16,4 +19,33 @@ class Walk < ApplicationRecord
     using: {
       tsearch: { prefix: true }
     }
+
+  def calc_distance
+    base_url= "https://api.mapbox.com/directions/v5/mapbox/walking/"
+    index = 0
+    25.times do
+      index += 1
+      unless self.walk_landmarks.find_by(landmark_order_no: index).nil?
+        target_lm = self.walk_landmarks.find_by(landmark_order_no: index).landmark
+        base_url += "#{target_lm.longitude},#{target_lm.latitude};"
+      end
+    end
+
+    api_setting_url = "?alternatives=false&continue_straight=true&geometries=geojson&overview=simplified&steps=false&access_token="
+    mapbox_key = ENV['MAPBOX_API_KEY']
+    map_api_url = base_url.chomp(';') + api_setting_url + mapbox_key
+
+    dist_hash = URI.open(base_url)
+    parsd = JSON.parse(dist_hash.string)
+    routes = parsd["routes"]
+    routes_1 = routes[0]
+    distance = routes_1["distance"]
+    # self.distance = distance
+    # self.save
+    return distance
+  end
+
+  def distance_to_km
+    self.distance.to_f / 1000
+  end
 end
