814cc1aa9f79561f9a5fee800055a546cd41de38
Hunter Bourgeois <bahunter@gmail.com>
2021-11-29 18:08:22 +0100

    seeds fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@ User.destroy_all
 puts "Creating Walks and Users"
 
 user1 = User.new
-# user1.first_name = 'Sally'
+# user1.username = 'SallyS'
 # user1.last_name = 'Secret'
-user1.email = 'user1@walkingstick.com'
+user1.email = 'Sally.Secret@walkingstick.com'
 user1.password = '123456'
 # user1.password_confirmation = '123456'
 # user1.encrypted_password = '123456'
@@ -66,7 +66,7 @@ landmark.creator = user1
 landmark.title = 'Alfonso XII Monument'
 landmark.latitude = '40.417440504018366'
 landmark.longitude = '-3.683049973659502'
-landmark.description = 'The largest monument you’ll see in El Retiro Park in Madrid is this hemicycle located on the east side of the lake. This majestic work was commissioned in 1875 by María Cristina, widow of Alfonso XII, as a tribute to her late husband. The monument of Alfonso XII riding a horse represents peace, freedom and progress. But it also has, in its internal face, symbols of the Army of Montserrat and the science, art and industry.',
+landmark.description = 'The largest monument you’ll see in El Retiro Park in Madrid is this hemicycle located on the east side of the lake. This majestic work was commissioned in 1875 by María Cristina, widow of Alfonso XII, as a tribute to her late husband. The monument of Alfonso XII riding a horse represents peace, freedom and progress. But it also has, in its internal face, symbols of the Army of Montserrat and the science, art and industry.'
 file = File.open('app/assets/images/alfonsoXII.jpg')
 landmark.photo.attach(io: file, filename: 'alfonsoXII.jpg', content_type: 'image/jpg')
 landmark.creator = user1
@@ -90,7 +90,7 @@ landmark.creator = user1
 landmark.title = ' La Rosaleda '
 landmark.latitude = '40.410904446810775'
 landmark.longitude = '-3.6803869755084424'
-landmark.description = 'La Rosaleda is an elliptical space, delimited by cypresses and with 4 entrances. Inspired by the great European capitals, the mayor of Madrid Carlos Prat commissioned Cecilio Rodríguez to build it. The model to follow was the Gardens of Bologna in the city of Paris, an inspiration that came to Cecilio Rodríguez on a trip to the French city from which he returned with 12,000 roses of more than 350 different species.',
+landmark.description = 'La Rosaleda is an elliptical space, delimited by cypresses and with 4 entrances. Inspired by the great European capitals, the mayor of Madrid Carlos Prat commissioned Cecilio Rodríguez to build it. The model to follow was the Gardens of Bologna in the city of Paris, an inspiration that came to Cecilio Rodríguez on a trip to the French city from which he returned with 12,000 roses of more than 350 different species.'
 file = File.open('app/assets/images/rosaleda.png')
 landmark.photo.attach(io: file, filename: 'rosaleda.png', content_type: 'image/png')
 landmark.creator = user1
@@ -114,7 +114,7 @@ landmark.creator = user1
 landmark.title = 'Casita del Pescador'
 landmark.latitude = '40.420711165978'
 landmark.longitude = '-3.6809420448805135'
-landmark.description = 'The Casita del Pescador is a small historical house, located in the northwest part of El Retiro Park in Madrid. It was in the beginning of the XIX century when this house was rebuilt by Fernando VII after the French invasion. The “Salón Persa” (Persian Hall) and the Casita del Pescador were restored and were renowned as “Los Caprichos” (The Caprices). The building was designed by Isidro González Velázquez so that the monarch and his friends could enjoy fishing days in a relaxed atmosphere.',
+landmark.description = 'The Casita del Pescador is a small historical house, located in the northwest part of El Retiro Park in Madrid. It was in the beginning of the XIX century when this house was rebuilt by Fernando VII after the French invasion. The “Salón Persa” (Persian Hall) and the Casita del Pescador were restored and were renowned as “Los Caprichos” (The Caprices). The building was designed by Isidro González Velázquez so that the monarch and his friends could enjoy fishing days in a relaxed atmosphere.'
 file = File.open('app/assets/images/casapescador.png')
 landmark.photo.attach(io: file, filename: 'casapescador.png', content_type: 'image/png')
 landmark.creator = user1
@@ -138,7 +138,7 @@ landmark.creator = user1
 landmark.title = 'Estatua del Angel Caido'
 landmark.latitude = '40.41115997729375'
 landmark.longitude = '-3.6825368025521836'
-landmark.description = 'The Estatua del Ángel Caído it’s a work designed in 1877 by the renowned Madrid sculptors Ricardo Bellver and Francisco Jareño. This impressive monument is located on top of a Chinese porcelain pedestal, which was destroyed after the Independence War. The statue, which represents the expulsion of the angel Lucifer to hell, is one of the most emblematic works of El Retiro Park in Madrid.',
+landmark.description = 'The Estatua del Ángel Caído it’s a work designed in 1877 by the renowned Madrid sculptors Ricardo Bellver and Francisco Jareño. This impressive monument is located on top of a Chinese porcelain pedestal, which was destroyed after the Independence War. The statue, which represents the expulsion of the angel Lucifer to hell, is one of the most emblematic works of El Retiro Park in Madrid.'
 file = File.open('app/assets/images/angel.png')
 landmark.photo.attach(io: file, filename: 'angel.png', content_type: 'image/png')
 landmark.creator = user1
