1c2b2223660acea55c5e3407ae629fb5e68f6340
Gonzalo Guerra <cryptogon@outlook.com>
2021-12-01 13:30:58 +0100

    fuckery revision needed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def show
+      def show
     @user = User.find(params[:id])
     @walks = Walk.all
     @random_walks = @walks.sample(2)
   end
 
-
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'pages#home'
-  get '/dashboard', to: 'pages#dashboard'
-  get '/walks/:walk_id/:landmark_order_no', to: 'walks#navigate'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: %i[show]
+  # resources :users, only: %i[show]
   resources :walks, only: [:show, :index] do
-  resources :landmarks, only: [:show, :index]
+    resources :landmarks, only: [:show, :index]
   end
 
+  get '/dashboard', to: 'pages#dashboard'
+  get '/walks/:walk_id/:landmark_order_no', to: 'walks#navigate'
 end
